8ba76bf949ac8859ae44ef36f5396eaab288559e
Gabriellpa <43101962+Gabriellpa@users.noreply.github.com>
2020-03-25 20:16:35 -0300

    fix: url for raw data of total deaths fixed

--- src/main/java/com/spring/cov19/clients/Cov19DataClient.java
+++ src/main/java/com/spring/cov19/clients/Cov19DataClient.java
@@ -7,7 +7,7 @@ public interface Cov19DataClient {
 	@RequestLine("GET CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
     String confirmedCases();
 	
-	@RequestLine("GET CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
+	@RequestLine("GET CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
 	String deathsCases();
 	
 	@RequestLine("GET CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv")
